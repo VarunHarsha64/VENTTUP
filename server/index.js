209c2cf6347d994a612e2f5914c699cfea3fd776
Varun Harsha <varunharsha10@gmail.com>
2024-11-14 16:10:33 +0530

    Frontend deployment and cors modification

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://venttup-client.vercel.app", credentials: true }));
 app.use(express.json());
 
 app.post(
